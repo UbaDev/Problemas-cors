009e164dc98bc70c55b21cb52129bf1b7678badd
Ubaldo Aviles <ubaldoaviles@Ubaldos-Laptop.local>
2024-01-29 14:12:01 -0600

    mi app cors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState("");
+  const [error, setError] = useState("");
+
+const handleSearch = () => {
+  fetch(
+    `https://cors-anywhere.herokuapp.com/https://es.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${searchTerm}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Problema CORS: No se pudo completar la solicitud.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const firstResult = data?.query?.search?.[0];
+      setSearchResult(firstResult);
+      setError("");
+    })
+    .catch((error) => {
+      setSearchResult("");
+      setError(error.message);
+    });
+};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Contenedor del buscador */}
+        <div style={{ width: "50%", textAlign: "center" }}>
+          <input
+            type="text"
+            placeholder="Buscar en Wikipedia"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button onClick={handleSearch}>Buscar</button>
+        </div>
+
+        {/* Pantalla de resultados y errores */}
+        {searchResult && (
+          <div
+            style={{
+              marginTop: "20px",
+              padding: "10px",
+              border: "1px solid #ccc",
+            }}
+          >
+            <h3>Resultados de la búsqueda:</h3>
+            <p>{JSON.stringify(searchResult, null, 2)}</p>
+          </div>
+        )}
+        {error && (
+          <div
+            style={{
+              marginTop: "20px",
+              padding: "10px",
+              border: "1px solid #ff0000",
+              color: "#ff0000",
+            }}
+          >
+            <h3>Error:</h3>
+            <p>{error}</p>
+          </div>
+        )}
       </header>
     </div>
   );
